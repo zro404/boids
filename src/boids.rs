185f90f82c38e9fe6701db8c2b08c1312633486f
Harish <dev.harish3124@gmail.com>
2025-09-20 23:50:04 +0530

    Add Seperation and Cohesion rules
    TODO: Replace teleport at simulation boundary with boundary avoidance

--- src/boids.rs
+++ src/boids.rs
@@ -1,7 +1,10 @@
-use bevy::{math::VectorSpace, prelude::*, reflect::EnumInfo};
+use bevy::prelude::*;
 use rand::random;
 
-use crate::constants::{BOID_COUNT, BOID_DIAMETER, BOID_SPEED, HALF_WORLD_SIZE, WORLD_SIZE};
+use crate::constants::{
+    AVOIDANCE_THRESHOLD, BOID_COUNT, BOID_DIAMETER, BOID_SPEED, HALF_WORLD_SIZE,
+    LOCAL_FLOCK_RADIUS, WORLD_SIZE,
+};
 
 #[derive(Component)]
 pub struct Boid {
@@ -19,7 +22,11 @@ pub fn spawn_boids(
             Boid { velocity: vel },
             Mesh3d(meshes.add(Sphere::new(BOID_DIAMETER / 2.0))),
             MeshMaterial3d(materials.add(Color::WHITE)),
-            Transform::from_xyz(0.0, 0.5, 0.0),
+            // Transform::from_xyz(0.0, 0.5, 0.0),
+            Transform {
+                translation: gen_boid_init_pos(),
+                ..default()
+            },
         ));
     }
 }
@@ -33,8 +40,15 @@ fn gen_boid_init_vel() -> Vec3 {
     return v;
 }
 
-pub fn move_boids(mut query: Query<(&mut Transform, &Boid), With<Boid>>) {
-    for (mut t, b) in &mut query {
+fn gen_boid_init_pos() -> Vec3 {
+    let unit = || {
+        return random::<f32>() * WORLD_SIZE - HALF_WORLD_SIZE;
+    };
+    return Vec3::new(unit(), unit(), unit());
+}
+
+pub fn move_boids(mut query: Query<(&mut Transform, &mut Boid), With<Boid>>) {
+    for (mut t, mut b) in &mut query {
         if t.translation.x > HALF_WORLD_SIZE {
             t.translation.x -= WORLD_SIZE;
         } else if t.translation.x < -HALF_WORLD_SIZE {
@@ -50,58 +64,53 @@ pub fn move_boids(mut query: Query<(&mut Transform, &Boid), With<Boid>>) {
         } else if t.translation.z < -HALF_WORLD_SIZE {
             t.translation.z += WORLD_SIZE;
         }
+
+        // if t.translation.x > HALF_WORLD_SIZE || t.translation.x < -HALF_WORLD_SIZE {
+        //     b.velocity.x = -b.velocity.x;
+        // }
+        // if t.translation.y > HALF_WORLD_SIZE || t.translation.y < -HALF_WORLD_SIZE {
+        //     b.velocity.y = -b.velocity.y;
+        // }
+        // if t.translation.z > HALF_WORLD_SIZE || t.translation.z < -HALF_WORLD_SIZE {
+        //     b.velocity.z = -b.velocity.z;
+        // }
+
         t.translation += b.velocity;
     }
 }
 
 pub fn simulate(mut query: Query<(&mut Transform, &mut Boid), With<Boid>>) {
-    let mut velVec = Vec::<Vec3>::new();
+    let mut vel_vec = Vec::<Vec3>::new();
+
     for (t, b) in &query {
         let mut v_sum = Vec3::ZERO;
+        let mut c = Vec3::ZERO;
+        let mut t_sum = Vec3::ZERO;
+
         for (t1, b1) in &query {
-            v_sum += b1.velocity.normalize();
+            let dist = t.translation.distance(t1.translation);
+
+            if dist <= LOCAL_FLOCK_RADIUS {
+                v_sum += b1.velocity.normalize();
+                t_sum += t1.translation.normalize();
+            }
+
+            if dist <= AVOIDANCE_THRESHOLD {
+                c -= t1.translation - t.translation;
+            }
         }
         let mean_v = (v_sum.normalize()) * BOID_SPEED;
-        velVec.push(b.velocity.lerp(mean_v, 0.001));
-        // b.velocity = b.velocity.lerp(mean_v, 0.001);
 
-        // println!("{} {} {}", v_sum, mean_v, b.velocity.lerp(mean_v, 0.001));
+        let mut new_vel = b.velocity.lerp(mean_v, 0.1);
+        new_vel += c;
+        new_vel += b.velocity.lerp(t_sum, 0.1);
+
+        vel_vec.push(new_vel.normalize() * BOID_SPEED);
     }
+
     let mut i: usize = 0;
-    for (mut t, mut b) in &mut query {
-        b.velocity = velVec[i];
+    for (_, mut b) in &mut query {
+        b.velocity = vel_vec[i];
         i += 1;
     }
-
-    // let mut iter = query.iter_mut();
-    // while let Some((mut t, mut b)) = iter.next() {
-    //     let mut v_sum = Vec3::ZERO;
-    //     for (t1, b1) in iter.remaining_mut() {
-    //         v_sum += b1.velocity;
-    //     }
-    //     let mean_v = ((v_sum).normalize()) * BOID_SPEED;
-    //     println!("{} {} {}", v_sum, mean_v, b.velocity.lerp(mean_v, 0.001));
-    //     // break;
-    //     // b.velocity = b.velocity.lerp(mean_v, 0.001);
-    //     // b.velocity = b.velocity.lerp(mean_v, 0.001);
-    // }
-
-    // let mut entities: Vec<(Mut<Transform>, Mut<Boid>)> = query.iter_mut().collect();
-    // for i in 0..BOID_COUNT {
-    // let (left_vec, right_vec) = entities.split_at_mut(i);
-    // do calc for left sub vec
-    // b.velocity = alignment(&b.velocity, left_vec);
-
-    // do calc for right sub vec
-    // b.velocity = alignment(&b.velocity, right_vec);
-    // }
-    // while let Some([(mut t, mut b), (mut t1, mut b1)]) = combinations.fetch_next() {}
-    // let mut v_sum = Vec3::ZERO;
-    // for (t, b) in &mut query {
-    //     v_sum += b.velocity.normalize();
-    // }
-    // let mean_v = ((v_sum / (BOID_COUNT as f32)).normalize()) * BOID_SPEED;
-    // for (mut t, mut b) in &mut query {
-    //     b.velocity = b.velocity.lerp(mean_v, 0.001);
-    // }
 }
--- src/constants.rs
+++ src/constants.rs
@@ -1,19 +1,17 @@
 use bevy::math::Vec3;
 
-pub const WORLD_SIZE: f32 = 100.0;
+pub const WORLD_SIZE: f32 = 200.0;
 
 pub const CAM_SPEED: f32 = 0.02;
 
-pub const BOID_COUNT: usize = 10;
+pub const BOID_COUNT: usize = 1000;
 pub const BOID_SPEED: f32 = 0.5;
 pub const BOID_DIAMETER: f32 = 1.0;
 
+pub const AVOIDANCE_THRESHOLD: f32 = 2.5;
+pub const LOCAL_FLOCK_RADIUS: f32 = 10.0;
+
 // Following exist for reducing computations
-pub const CAMERA_POS: Vec3 = Vec3::new(
-    WORLD_SIZE + 100.0,
-    (WORLD_SIZE + 100.0) / 2.0,
-    (WORLD_SIZE + 100.0) / 2.0,
-);
+pub const CAMERA_POS: Vec3 = Vec3::new(WORLD_SIZE * 2.0, WORLD_SIZE, WORLD_SIZE);
 
 pub const HALF_WORLD_SIZE: f32 = (WORLD_SIZE + BOID_DIAMETER) / 2.0;
-pub const WORLD_VEC: Vec3 = Vec3::new(WORLD_SIZE, WORLD_SIZE, WORLD_SIZE);
